9475a4338b2637ac9c7c5cb4662d428644968891
Flynecraft <flynn.kresslein@gmx.de>
2023-10-24 18:36:11 +0200

    asked questions channel + ignore old channels

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import disnake
 from disnake.ext import commands, tasks
 from bottoken import TOKEN
 import variables
+import time
 
 intents = disnake.Intents.all()
 
@@ -27,32 +28,38 @@ bot.add_cog(RedirectCommand(bot))
 @tasks.loop(minutes=10)
 async def ten():
     print("Starting query")
-    channel = bot.get_channel(variables.stats)
-    total = 0
+    channel_resolved = bot.get_channel(variables.stats)
+    channel_asked = bot.get_channel(variables.stats_asked)
+    total_threads = 0
     resolved_threads = 0
     for i in variables.help_channels:
         questions = bot.get_channel(i).threads.__len__()
         archived_qns = await bot.get_channel(i).archived_threads(limit=None).flatten()
         for thread in bot.get_channel(i).threads:
+            total_threads += 1
             resolved_tag = thread.parent.get_tag_by_name("RESOLVED")
             if resolved_tag in thread.applied_tags:
                     resolved_threads += 1
         questions = questions + archived_qns.__len__()
 
         for thread in archived_qns:
+            
+            creation_unix = time.mktime(thread.create_timestamp.timetuple())
             parent_channel = thread.parent
             resolved_tag = parent_channel.get_tag_by_name("RESOLVED")
-            
-            if resolved_tag in thread.applied_tags:
-                resolved_threads += 1
+            if creation_unix >= 1674615000:
+                total_threads += 1
+                if resolved_tag in thread.applied_tags:
+                    resolved_threads += 1
 
-        total = total + questions
-
-    await channel.edit(name=f"Resolved Questions: {resolved_threads}")
-    print(f"Finshed {resolved_threads}/{total}")
+    await channel_resolved.edit(name=f"Resolved Questions: {resolved_threads}")
+    await channel_asked.edit(name=f"Asked Questions: {total_threads}")
+    print(f"Finshed {resolved_threads}/{total_threads}")
     
 @bot.event
 async def on_ready():
     await ten.start()
 
 bot.run(TOKEN)
+
+# oldest resolved thread timestamp 1674615000
