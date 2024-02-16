bdfc7da3cba638a95c427059abc5e2c17aef7d4b
HeDeAn <tannenbaumimjuli@gmail.com>
2024-02-16 19:21:35 +0100

    updated the purge command to log the deleted messages with author

--- commands/purge.py
+++ commands/purge.py
@@ -1,5 +1,7 @@
-from disnake import ApplicationCommandInteraction, Option
+import io
+from disnake import ApplicationCommandInteraction, Option, Embed, Colour, File
 from disnake.ext import commands
+import variables
 
 
 class PurgeCommand(commands.Cog):
@@ -18,11 +20,41 @@ class PurgeCommand(commands.Cog):
             )
         ]
     )
+    # Deletes a specified number of messages from the channel and logs the action
     async def purge(self, inter: ApplicationCommandInteraction, limit: int):
-        """Deletes a specified number of messages from the channel."""
+        # Stops the purge if the purge amount is over the API's limit
         if limit > 100:
             await inter.response.send_message("You cannot delete more than 100 messages at once", ephemeral=True)
             return
 
-        deleted = await inter.channel.purge(limit=limit)
-        await inter.response.send_message(f"{len(deleted)} messages have been deleted")
+        # Deletes the messages
+        deleted_messages = await inter.channel.purge(limit=limit)
+
+        # Logs the purge action
+        log_embed = Embed(
+            color=Colour.orange(),
+            title="**` /purge` Command**",
+            description=f"{inter.user.name} purged {len(deleted_messages)} messages in {inter.channel.mention}."
+        )
+        
+        # Add the autor and contents of the deleted messages to the log
+        if variables.send_log_messages_as_embed: # as an embed
+            for messages in deleted_messages:
+                log_embed.add_field(name="Autor", value=messages.author.name, inline=False)
+                log_embed.add_field(name="Content", value=messages.content, inline=False)
+        else: # as a txt file
+            file_content = io.StringIO()
+            for messages in deleted_messages:
+                file_content.write(f"Author: {messages.author.name}\nContent: {messages.content}\n\n")
+
+            file_content.seek(0)
+            file = File(fp=file_content, filename="purged messages.txt")
+            
+
+        log_channel_id = variables.logs
+        log_channel = await inter.guild.get_channel(log_channel_id)
+        if log_channel:
+            await log_channel.send(embed=log_embed, file=file)
+
+        # Confirm the purge
+        await inter.response.send_message(f"{len(deleted_messages)} messages have been deleted", ephemeral=True)
