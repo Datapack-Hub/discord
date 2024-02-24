9ab2a022aa392ab06f0a781c8a4824f386a77b6f
Silabear <56885288+Silabear@users.noreply.github.com>
2024-02-24 16:48:04 +0000

    remove top cmd message

--- commands/top.py
+++ commands/top.py
@@ -6,12 +6,11 @@ class TopCommand(commands.Cog, name="top"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(title="top", description="Jump to the top of the thread!")
+    @commands.slash_command(title="top", description="Jump to the top of the thread or channel!")
     async def top(self, inter: disnake.ApplicationCommandInteraction):
         message = await inter.channel.history(limit=1, oldest_first=True).flatten()
 
         await inter.response.send_message(
-            "Click the button below to jump to the top of this channel.",
             components=[
                 disnake.ui.Button(label="Jump to top", url=message[0].jump_url)
             ],
