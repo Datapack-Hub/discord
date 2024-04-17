ca6c964c624f05e530dbf7d9d8ac33b87ca9a950
Silabear <56885288+Silabear@users.noreply.github.com>
2024-04-17 18:20:38 +0100

    member comands

---
+++ commands/members.py
@@ -0,0 +1,15 @@
+import disnake
+from disnake.ext import commands
+
+
+class Members(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(name="members", description="View member list and count")
+    async def members(self, inter: disnake.ApplicationCommandInteraction):
+        with open("Member Names.txt","w") as file:
+            file.truncate()
+            file.writelines([member.name for member in inter.guild.members])
+            await inter.response.send_message(f"`{inter.guild.member_count}` members",file=disnake.File(file),ephemeral=True)
+            file.close()
