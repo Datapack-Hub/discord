a15649789227c5a5c841898c6ec8eb21917ccea8
Silabear <56885288+Silabear@users.noreply.github.com>
2024-04-17 18:29:30 +0100

    sticking your disnake.File(file) out to the rizzler

--- commands/members.py
+++ commands/members.py
@@ -11,8 +11,11 @@ class MembersCommand(commands.Cog):
         if inter.author.id == 880000911848636468:
             await inter.response.send_message("no flyne you do not deserve to use this command with your stinky private member list command",ephemeral=True)
         else:
-            open("Member Names.txt","w+").close()
+            open("Member Names.txt","w").close()
             with open("Member Names.txt","w+") as file:
                 file.writelines([member.name for member in inter.guild.members])
-                await inter.response.send_message(f"`{inter.guild.member_count}` members",file=disnake.File(file),ephemeral=True)
                 file.close()
+            
+            file = open("Member Names.txt","r")
+            await inter.response.send_message(f"`{inter.guild.member_count}` members",file=disnake.File(file),ephemeral=True)
+            file.close()
