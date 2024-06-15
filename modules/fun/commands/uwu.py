741bad6546e4cd191ce8530f2a119046fb5a5978
Silabear <56885288+Silabear@users.noreply.github.com>
2024-06-15 18:39:52 +0100

    session based uwufiage

--- modules/fun/commands/uwu.py
+++ modules/fun/commands/uwu.py
@@ -7,6 +7,7 @@ from disnake.ext import commands
 class UwuCommand(commands.Cog, name="uwu"):
     def __init__(self, bot):
         self.bot = bot
+        self.data = {"users": [], "channels": []}
 
     @commands.slash_command(name="uwu")
     async def uwu(self, inter: disnake.ApplicationCommandInteraction):
@@ -29,44 +30,30 @@ class UwuCommand(commands.Cog, name="uwu"):
     @uwu.sub_command("auto","Enable the auto uwufier")
     async def auto(self, inter: disnake.ApplicationCommandInteraction):
         try:
-            with open("uwufied.json","r+") as fp:
-                data: dict = json.load(fp)
-                users = data["users"]
-                if inter.author.id in users:
-                    users.remove(inter.author.id)
-                    await inter.response.send_message("**Disabled** auto uwufier for user " + inter.author.mention)
-                    Log.info(f"Disabled the user uwufier for {inter.author.name}")
-                else:
-                    users.append(inter.author.id)
-                    await inter.response.send_message("**Enabled** auto uwufier for user " + inter.author.mention)
-                    Log.info(f"Enabled the user uwufier for {inter.author.name}")
-                data["users"] = users
-                with open("uwufied.json","w") as fp:
-                    fp.seek(0)
-                    json.dump(data,fp)
-                    fp.close()
-                fp.close()
+            users = self.data["users"]
+            if inter.author.id in users:
+                users.remove(inter.author.id)
+                await inter.response.send_message("**Disabled** auto uwufier for user " + inter.author.mention)
+                Log.info(f"Disabled the user uwufier for {inter.author.name}")
+            else:
+                users.append(inter.author.id)
+                await inter.response.send_message("**Enabled** auto uwufier for user " + inter.author.mention)
+                Log.info(f"Enabled the user uwufier for {inter.author.name}")
+            self.data["users"] = users
         except Exception as e:
             Log.error(f"Could not toggle uwufier for user {inter.author.name}: {e}")
     
     @uwu.sub_command("channel","Enable the auto uwufier for the current channel")
     async def channel(self, inter: disnake.ApplicationCommandInteraction):
         try:
-            with open("uwufied.json","r+") as fp:
-                data: dict = json.load(fp)
-                if inter.channel.id in data["channels"]:
-                    data["channels"].remove(inter.channel.id)
-                    await inter.response.send_message("**Disabled** auto uwufier for channel " + inter.channel.mention)
-                    Log.info(f"Disabled the user uwufier for #{inter.channel.name}")
-                else:
-                    data["channels"].append(inter.channel.id)
-                    await inter.response.send_message("**Enabled** auto uwufier for channel " + inter.channel.mention)
-                    Log.info(f"Enabled the user uwufier for #{inter.channel.name}")
-                with open("uwufied.json","w") as fp:
-                    fp.seek(0)
-                    json.dump(data,fp)
-                    fp.close()
-                fp.close()
+            if inter.channel.id in self.data["channels"]:
+                self.data["channels"].remove(inter.channel.id)
+                await inter.response.send_message("**Disabled** auto uwufier for channel " + inter.channel.mention)
+                Log.info(f"Disabled the user uwufier for #{inter.channel.name}")
+            else:
+                self.data["channels"].append(inter.channel.id)
+                await inter.response.send_message("**Enabled** auto uwufier for channel " + inter.channel.mention)
+                Log.info(f"Enabled the user uwufier for #{inter.channel.name}")
         except Exception as e:
             Log.error(f"Could not toggle uwufier for channel #{inter.channel.name}: {e}")
             
@@ -74,22 +61,33 @@ class UwuCommand(commands.Cog, name="uwu"):
     @uwu.sub_command("user","Enable the auto uwufier for another user")
     async def auto(self, inter: disnake.ApplicationCommandInteraction, user: disnake.Member):
         try:
-            with open("uwufied.json","r+") as fp:
-                data: dict = json.load(fp)
-                users = data["users"]
-                if user.id in users:
-                    users.remove(user.id)
-                    await inter.response.send_message("**Disabled** auto uwufier for user " + user.mention)
-                    Log.info(f"Disabled the user uwufier for {user.name}")
-                else:
-                    users.append(user.id)
-                    await inter.response.send_message("**Enabled** auto uwufier for user " + user.mention)
-                    Log.info(f"Enabled the user uwufier for {user.name}")
-                data["users"] = users
-                with open("uwufied.json","w") as fp:
-                    fp.seek(0)
-                    json.dump(data,fp)
-                    fp.close()
-                fp.close()
+            users = self.data["users"]
+            if user.id in users:
+                users.remove(user.id)
+                await inter.response.send_message("**Disabled** auto uwufier for user " + user.mention)
+                Log.info(f"Disabled the user uwufier for {user.name}")
+            else:
+                users.append(user.id)
+                await inter.response.send_message("**Enabled** auto uwufier for user " + user.mention)
+                Log.info(f"Enabled the user uwufier for {user.name}")
+            self.data["users"] = users
         except Exception as e:
             Log.error(f"Could not toggle uwufier for user {user.name}: {e}")
+        
+    @commands.Cog.listener()
+    async def on_message(self, message: disnake.Message):
+        if ((message.author.id in self.data["users"]) or (message.channel.id in self.data["channels"])) and message.channel.type == disnake.ChannelType.text and not message.author.bot:
+            # Create/get hook
+            hooks = await message.channel.webhooks()
+            for hook in hooks:
+                if hook.name == "DPH":
+                    break
+            else:
+                hook = await message.channel.create_webhook(name="DPH")
+
+            # Delete message
+            await message.delete()
+            
+            # The important bit
+            uwu = Uwuifier()
+            await hook.send(uwu.uwuify_sentence(message.content.lower()),wait=False,username=message.author.display_name,avatar_url=message.author.display_avatar.url,allowed_mentions=disnake.AllowedMentions.none())
--- modules/fun/listeners.py
+++ modules/fun/listeners.py
@@ -29,21 +29,3 @@ class FunListeners(commands.Cog):
             uwu = Uwuifier()
             try: await hook.send(uwu.uwuify_sentence(message.content.lower()),wait=False,username=message.author.display_name,avatar_url=message.author.display_avatar.url,allowed_mentions=disnake.AllowedMentions.none())
             except Exception as e: Log.error(f"Could not uwufy message from {message.author.name}: {e}")
-        with open("uwufied.json","r") as fp:
-            data = json.load(fp)
-            if ((message.author.id in data["users"]) or (message.channel.id in data["channels"])) and message.channel.type == disnake.ChannelType.text and not message.author.bot:
-                # Create/get hook
-                hooks = await message.channel.webhooks()
-                for hook in hooks:
-                    if hook.name == "DPH":
-                        break
-                else:
-                    hook = await message.channel.create_webhook(name="DPH")
-
-                # Delete message
-                await message.delete()
-                
-                # The important bit
-                uwu = Uwuifier()
-                await hook.send(uwu.uwuify_sentence(message.content.lower()),wait=False,username=message.author.display_name,avatar_url=message.author.display_avatar.url,allowed_mentions=disnake.AllowedMentions.none())
-            fp.close()
