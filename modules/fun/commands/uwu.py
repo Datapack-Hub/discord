ef8a603984da3e47192c8d67c66f5f359c32a606
Silabear <56885288+Silabear@users.noreply.github.com>
2024-06-15 17:52:22 +0100

    let me uwufiy other people

--- modules/fun/commands/uwu.py
+++ modules/fun/commands/uwu.py
@@ -1,5 +1,6 @@
 import json
 from utils.uwufier import Uwuifier
+import utils.log as Log
 import disnake
 from disnake.ext import commands
 
@@ -22,38 +23,73 @@ class UwuCommand(commands.Cog, name="uwu"):
             hook = await inter.channel.create_webhook(name="DPH")
         uwu = Uwuifier()
         await hook.send(uwu.uwuify_sentence(text.lower()),wait=False,username=inter.author.display_name,avatar_url=inter.author.display_avatar.url,allowed_mentions=disnake.AllowedMentions.none())
-        await inter.response.send_message("yes done")
+        await inter.response.send_message("yes done",ephemeral=True)
+        Log.info(f"Uwufied a message from {inter.author.name} in #{inter.channel.name}")
     
     @uwu.sub_command("auto","Enable the auto uwufier")
     async def auto(self, inter: disnake.ApplicationCommandInteraction):
-        with open("uwufied.json","r+") as fp:
-            data: dict = json.load(fp)
-            users = data["users"]
-            if inter.author.id in users:
-                users.remove(inter.author.id)
-                await inter.response.send_message("**Disabled** auto uwufier for user " + inter.author.mention)
-            else:
-                users.append(inter.author.id)
-                await inter.response.send_message("**Enabled** auto uwufier for user " + inter.author.mention)
-            data["users"] = users
-            with open("uwufied.json","w") as fp:
-                fp.seek(0)
-                json.dump(data,fp)
+        try:
+            with open("uwufied.json","r+") as fp:
+                data: dict = json.load(fp)
+                users = data["users"]
+                if inter.author.id in users:
+                    users.remove(inter.author.id)
+                    await inter.response.send_message("**Disabled** auto uwufier for user " + inter.author.mention)
+                    Log.info(f"Disabled the user uwufier for {inter.author.name}")
+                else:
+                    users.append(inter.author.id)
+                    await inter.response.send_message("**Enabled** auto uwufier for user " + inter.author.mention)
+                    Log.info(f"Enabled the user uwufier for {inter.author.name}")
+                data["users"] = users
+                with open("uwufied.json","w") as fp:
+                    fp.seek(0)
+                    json.dump(data,fp)
+                    fp.close()
                 fp.close()
-            fp.close()
+        except Exception as e:
+            Log.error(f"Could not toggle uwufier for user {inter.author.name}: {' '.join(e.args)}")
     
     @uwu.sub_command("channel","Enable the auto uwufier for the current channel")
     async def channel(self, inter: disnake.ApplicationCommandInteraction):
-        with open("uwufied.json","r+") as fp:
-            data: dict = json.load(fp)
-            if inter.channel.id in data["channels"]:
-                data["channels"].remove(inter.channel.id)
-                await inter.response.send_message("**Disabled** auto uwufier for channel " + inter.channel.mention)
-            else:
-                data["channels"].append(inter.channel.id)
-                await inter.response.send_message("**Enabled** auto uwufier for channel " + inter.channel.mention)
-            with open("uwufied.json","w") as fp:
-                fp.seek(0)
-                json.dump(data,fp)
+        try:
+            with open("uwufied.json","r+") as fp:
+                data: dict = json.load(fp)
+                if inter.channel.id in data["channels"]:
+                    data["channels"].remove(inter.channel.id)
+                    await inter.response.send_message("**Disabled** auto uwufier for channel " + inter.channel.mention)
+                    Log.info(f"Disabled the user uwufier for #{inter.channel.name}")
+                else:
+                    data["channels"].append(inter.channel.id)
+                    await inter.response.send_message("**Enabled** auto uwufier for channel " + inter.channel.mention)
+                    Log.info(f"Enabled the user uwufier for #{inter.channel.name}")
+                with open("uwufied.json","w") as fp:
+                    fp.seek(0)
+                    json.dump(data,fp)
+                    fp.close()
                 fp.close()
-            fp.close()
+        except Exception as e:
+            Log.error(f"Could not toggle uwufier for channel #{inter.channel.name}: {' '.join(e.args)}")
+            
+        
+    @uwu.sub_command("user","Enable the auto uwufier for another user")
+    async def auto(self, inter: disnake.ApplicationCommandInteraction, user: disnake.Member):
+        try:
+            with open("uwufied.json","r+") as fp:
+                data: dict = json.load(fp)
+                users = data["users"]
+                if user.id in users:
+                    users.remove(user.id)
+                    await inter.response.send_message("**Disabled** auto uwufier for user " + user.mention)
+                    Log.info(f"Disabled the user uwufier for {user.name}")
+                else:
+                    users.append(user.id)
+                    await inter.response.send_message("**Enabled** auto uwufier for user " + user.mention)
+                    Log.info(f"Enabled the user uwufier for {user.name}")
+                data["users"] = users
+                with open("uwufied.json","w") as fp:
+                    fp.seek(0)
+                    json.dump(data,fp)
+                    fp.close()
+                fp.close()
+        except Exception as e:
+            Log.error(f"Could not toggle uwufier for user {user.name}: {' '.join(e.args)}")
---
+++ utils/log.py
@@ -0,0 +1,56 @@
+class Colour:
+    class text:
+        BLACK = '\033[30m'
+        RED = '\033[31m'
+        GREEN = '\033[32m'
+        YELLOW = '\033[33m'
+        BLUE = '\033[34m'
+        MAGENTA = '\033[35m'
+        CYAN = '\033[36m'
+        WHITE = '\033[37m'
+        class bright:
+            BLACK = '\033[90m'
+            RED = '\033[91m'
+            GREEN = '\033[92m'
+            YELLOW = '\033[93m'
+            BLUE = '\033[94m'
+            MAGENTA = '\033[95m'
+            CYAN = '\033[96m'
+            WHITE = '\033[97m'
+
+    class bg:
+        BLACK = '\033[40m'
+        RED = '\033[41m'
+        GREEN = '\033[42m'
+        YELLOW = '\033[43m'
+        BLUE = '\033[44m'
+        MAGENTA = '\033[45m'
+        CYAN = '\033[46m'
+        WHITE = '\033[47m'
+        class bright:
+            BLACK = '\033[100m'
+            RED = '\033[101m'
+            GREEN = '\033[102m'
+            YELLOW = '\033[103m'
+            BLUE = '\033[104m'
+            MAGENTA = '\033[105m'
+            CYAN = '\033[106m'
+            WHITE = '\033[107m'
+
+    # Reset code
+    RESET = '\033[0m'
+    
+def info(message: str):
+    print(f"{Colour.text.BLUE}INFO{Colour.RESET}  | " + message)
+    
+def debug(message: str):
+    print(f"{Colour.text.BLACK}DEBUG{Colour.RESET} | " + message)
+    
+def warn(message: str):
+    print(f"{Colour.text.YELLOW}WARN{Colour.RESET}  | " + message)
+
+def error(message: str):
+    print(f"{Colour.text.RED}ERROR{Colour.RESET} | " + message)
+    
+def fatal(message: str):
+    print(f"{Colour.bg.RED}FATAL{Colour.RESET} | " + message)
