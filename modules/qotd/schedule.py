d851491b52701484d438dd285d949790cb88ac14
PuckiSilver <pucki.silver@gmail.com>
2024-09-26 15:31:06 +0200

    fix typo in comment

--- modules/qotd/schedule.py
+++ modules/qotd/schedule.py
@@ -14,7 +14,7 @@ def _seconds_until_oclock(hour: int):
 async def schedule_qotd(bot: InteractionBot):
     while True:
         await asyncio.sleep(_seconds_until_oclock(18))
-        # Everything past here will only be execute once a day at 18:00 (I don't know the timezone though)
+        # Everything past here will only be executed once a day at 18:00 (In the server's timezone)
 
         # Find QOTD template to post
         input_channel = bot.get_channel(qotd_input_channel)
