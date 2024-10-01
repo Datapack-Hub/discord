1210e99045e7823a7099e7e0307782465239b230
Silabear <56885288+Silabear@users.noreply.github.com>
2024-10-01 18:33:16 +0100

    clean up the code

--- modules/qotd/schedule.py
+++ modules/qotd/schedule.py
@@ -19,15 +19,25 @@ async def schedule_qotd(bot: InteractionBot):
         # Find QOTD template to post
         input_channel = bot.get_channel(qotd_input_channel)
         question_short, question_long, question_author = None, None, None
+        
         async for message in input_channel.history(limit=200):
+            # If already posted, skip
             if '✅' in message.reactions or type(message.content) != str or message.author.bot:
                 continue
+            
+            # Check it follows the correct format
             content = str(message.content).split('\n')
             if len(content) != 2:
                 continue
+            
+            # Store in variables
             question_short, question_long = content[0], content[1]
             question_author = message.author
+            
+            # Add reaction to mark it as posted
             await message.add_reaction('✅')
+            
+            # End here
             break
         if question_short is None or question_long is None or question_author is None:
             input_channel.send("Help, I couldn't post a QOTD today! @everyone")
@@ -40,10 +50,10 @@ async def schedule_qotd(bot: InteractionBot):
             name=f'{qotd_day}. {question_short}',
             embed=Embed(
                 colour=Colour.orange(),
-                title=question_long,
-                description=f'<@&{qotd_role}>',
+                title=question_long
             ).set_footer(
                 text=("Suggested by " + question_author.name),
                 icon_url=question_author.avatar.url,
-            )
+            ),
+            content=f"<@&{qotd_role}>"
         )
